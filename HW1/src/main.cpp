e61e95dc7c0d46bd4bffec20207f5cb917dc6436
thecodehen <6582598+thecodehen@users.noreply.github.com>
2025-03-22 00:42:11 +0100

    hw1: Add json parser

--- HW1/src/main.cpp
+++ HW1/src/main.cpp
@@ -1,3 +1,33 @@
-int main() {
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <string>
+#include "json.hpp"
+using json = nlohmann::json;
+
+std::optional<json> parse_input_file(const std::string &input_file) {
+  std::ifstream file(input_file);
+  if (!file.is_open()) {
+    std::cerr << "Failed to open file: " << input_file << std::endl;
+    return std::nullopt;
+  }
+  json data = json::parse(file);
+  file.close();
+  return data;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <input file>" << std::endl;
+    return 1;
+  }
+
+  std::string input_file {argv[1]};
+  std::optional<json> data = parse_input_file(input_file);
+  if (!data) {
+    return 1;
+  }
+  std::cout << data.value().dump(4) << std::endl;
+
   return 0;
 }
