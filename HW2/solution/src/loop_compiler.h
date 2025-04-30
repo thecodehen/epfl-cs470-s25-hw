786fad7e5e95fc497e134a1656da3c602a5c3c39
thecodehen <6582598+thecodehen@users.noreply.github.com>
2025-04-30 16:35:37 +0200

    hw2: Add structure for loop_compiler

--- HW2/solution/src/loop_compiler.h
+++ HW2/solution/src/loop_compiler.h
@@ -11,6 +11,15 @@ public:
     LoopCompiler(const Program& program)
         : Compiler{program} {}
     VLIWProgram compile() override;
+private:
+    /** Returns length of instructions and maps to the bundle ID
+      */
+    std::vector<uint64_t> schedule(std::vector<Dependency>& dependencies) const;
+    std::vector<uint64_t> schedule_bb0(std::vector<uint64_t>& schedule);
+    std::vector<uint64_t> schedule_bb1(std::vector<uint64_t>& schedule);
+    std::vector<uint64_t> schedule_bb2(std::vector<uint64_t>& schedule);
+
+    // rename() const;
 };
 
 
