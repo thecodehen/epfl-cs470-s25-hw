4244e32d06f8989785152cacdba98c7f46b5018f
thecodehen <6582598+thecodehen@users.noreply.github.com>
2025-05-26 14:59:15 +0200

    hw3: Add faster kernel4

--- HW3/kernels/kernel4/kernel4.cpp
+++ HW3/kernels/kernel4/kernel4.cpp
@@ -1,11 +1,15 @@
 #include "kernel4.h"
 
+#define PARTITION_FACTOR 4
 void kernel4(int array[ARRAY_SIZE], int index[ARRAY_SIZE], int offset)
 {
+#pragma HLS ARRAY_PARTITION variable=array type=cyclic factor=PARTITION_FACTOR
+#pragma HLS ARRAY_PARTITION variable=index type=cyclic factor=PARTITION_FACTOR
     int tmp = 0;
     for (int i=offset+1; i<ARRAY_SIZE-1; ++i)
     {
 #pragma HLS PIPELINE
+#pragma HLS UNROLL factor=PARTITION_FACTOR
         tmp = tmp + index[i] * (array[i+1] - array[i]);
     }
     array[offset] = array[offset] + tmp;
