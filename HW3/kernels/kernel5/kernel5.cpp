a2742648bd3c09280e103734f0b3947dbb5d7bdd
thecodehen <6582598+thecodehen@users.noreply.github.com>
2025-05-26 19:45:43 +0200

    hw3: Merge two loops in kernel 5

--- HW3/kernels/kernel5/kernel5.cpp
+++ HW3/kernels/kernel5/kernel5.cpp
@@ -3,27 +3,16 @@
 #define PARTITION_FACTOR 8
 float kernel5(float bound, float a[ARRAY_SIZE], float b[ARRAY_SIZE])
 {
-    float sums[ARRAY_SIZE];
+    bool done = false;
 #pragma HLS ARRAY_PARTITION variable=a type=cyclic factor=PARTITION_FACTOR
 #pragma HLS ARRAY_PARTITION variable=b type=cyclic factor=PARTITION_FACTOR
-#pragma HLS ARRAY_PARTITION variable=sums type=cyclic factor=PARTITION_FACTOR
+    float sum;
     for (int i=0; i < ARRAY_SIZE; ++i) {
-#pragma HLS PIPELINE II=1
-#pragma HLS UNROLL factor=PARTITION_FACTOR
-        sums[i] = a[i] + b[i];
-    }
-    float sum = 0;
-
-    int j = 0;
-    bool done = false;
-    for (int i = 0; i < ARRAY_SIZE; i++) {
 #pragma HLS PIPELINE
 #pragma HLS UNROLL factor=PARTITION_FACTOR
-        if (!done && sums[i] < bound) {
-            ++j;
-        } else {
+        if (done || (sum = a[i] + b[i]) >= bound) {
             done = true;
         }
     }
-    return sums[j];
+    return sum;
 }
